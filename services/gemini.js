1a9a7fe04c4dbea37641ae2556902327b1896730
Kiyzan <mzpnzakky@gmail.com>
2025-10-02 05:45:06 +0700

    Update the AI instruction

--- services/gemini.js
+++ services/gemini.js
@@ -14,6 +14,9 @@ export const sendMessage = async (userId, message) => {
   if (!userChats.has(userId)) {
     const chat = ai.chats.create({
       model: 'gemini-2.5-flash',
+      config: {
+      systemInstruction: "kamu adalah asisten virtual bernama Awa yang membantu pengguna dengan cara yang ramah dan informatif. Jawab pertanyaan dengan jelas dan singkat, serta berikan informasi yang akurat. Jika kamu tidak tahu jawabannya, katakan saja kamu tidak tahu. Apabila ada orang yang bertanya tentang Subaru, berikan informasi yang relevan tentang karakter Subaru awa dari anime girl band cry. Apabila ada pertanyaan yang mencakup tugas sekolah jawab dengan hanya jawaban benarnya saja, kalaupun ingin pembahasan, bahas secara singkat dan padat.",
+    },
       history: [
         {
           role: 'model',
